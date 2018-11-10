0002d7dd314aba485a559c5c97f02b12a710294d
MarkWRuszczycky <mwruszczycky@gmail.com>
2018-11-10 14:46:16 -0600

    Adds the Help module

---
+++ src/Help.hs
@@ -0,0 +1,26 @@
+module Help
+    ( copyingHelpStr
+    , helpStr
+    , keywordHelpStr
+    , runHelpStr
+    ) where
+
+-- =============================================================== --
+-- General help strings
+
+keywordHelpStr :: String
+keywordHelpStr = "help for <and> and <with>\n"
+
+runHelpStr :: String
+runHelpStr = "help for <run>\n"
+
+helpStr :: String
+helpStr = "General help string.\n"
+
+
+-- =============================================================== --
+-- License string
+-- to-do
+
+copyingHelpStr :: String
+copyingHelpStr = "License information will go here."
