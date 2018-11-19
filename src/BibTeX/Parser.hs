36e2295547fa5a9f7f5fe93599a0801cebf0d546
MarkWRuszczycky <mwruszczycky@gmail.com>
2018-11-18 20:21:12 -0600

    Improves and refactors the parser
    1. Fairly large refactor of the parser.
    2. It now does not fail if there are @-symbols in the ignored
       comments. It is more forgiving of spaces in the entry type and key
       declarations. The code is significantly cleaned up in general.
    3. A more informative help string is added for files that fail to
       parse including an explanation of the current limitations of the
       parser with respect to the BibTeX standard.

--- src/BibTeX/Parser.hs
+++ src/BibTeX/Parser.hs
@@ -6,213 +6,209 @@ module BibTeX.Parser
     ) where
 
 import qualified Data.Attoparsec.Text as At
-import Data.Attoparsec.Text ( (<?>) )
-import Control.Applicative ( (<|>) )
-import Data.Text (Text, pack)
-import Data.Char ( isAlphaNum )
-import qualified Types as T
-import Data.Map.Strict as M
+import qualified Data.Text            as Tx
+import qualified Data.Map.Strict      as M
+import qualified Types                as T
+import Data.Bifunctor                       ( bimap       )
+import Control.Applicative                  ( (<|>)       )
+import Data.Text                            ( Text, pack  )
+import Data.Char                            ( isAlphaNum  )
+
+-- =============================================================== --
+-- Types
+
+-- |Convenience data type for entry-key/entry pairs.
+type KeyEntry = (Text, T.Entry)
+
+-- =============================================================== --
+-- Main parsers
+
+---------------------------------------------------------------------
+-- Full bibliography parser
+
+-- exported
 
 parseBib :: FilePath -> Text -> Either String T.Bibliography
 -- ^Exposed parser takes the file path to the .bib file and the input
 -- text to parse.
-parseBib fp x = do
-    refs <- At.parseOnly bibParser x
-    return T.Bibliography { T.path = fp
-                          , T.refs = refs }
+parseBib fp = bimap (errorMessage fp) go . At.parseOnly bibParser
+    where go x = T.Bibliography { T.path = fp
+                                , T.refs = x  }
 
-parseRef :: FilePath -> Text -> Either String T.Ref
-parseRef fp x = do
-    (k, v) <- At.parseOnly oneRef x
-    return $ T.Ref fp k v
-
----------------------------------------------------------------------
--- Hidden helper functions
----------------------------------------------------------------------
-
-type KeyEntry = (Text, T.Entry)
-
----------------------------------------------------------------------
--- Main parser
+errorMessage :: FilePath -> String -> String
+errorMessage fp err = unlines hs
+    where hs = [ "Unable to parse " ++ fp ++ " as a BibTeX bibliography"
+                 ++ " (.bib) file."
+               , "The btx .bib parser is still fairly rudimentary and does not"
+               , "yet support all the BibTeX features. In particular:"
+               , "    1. Be sure that you are using the braced format."
+               , "    2. Make sure that all comments are preceeded by a"
+               , "       %-symbol just like in LaTeX."
+               , "    3. The parser does not yet support @PREAMBLE or"
+               , "       @COMMENT entries."
+               , "    4. The parser does not yet support use of the"
+               , "       #-concatenation operator.\n"
+               , "Additional information from the parser: " ++ err
+               ]
 
 bibParser :: At.Parser T.References
 -- ^Main file parser for reading each BibTeX reference.
 bibParser = do
-    At.skipWhile ( /= '@' )
+    ignored
     rs <- At.many' reference
     At.endOfInput
     return . M.fromList $ rs
 
+---------------------------------------------------------------------
+-- Single reference parser
+-- Used for parsing doi dowloads and externally edited references.
+
+-- exported
+
+parseRef :: FilePath -> Text -> Either String T.Ref
+-- ^Parse a single BibTeX entry from a Text string.
+parseRef fp x = do
+    (k, v) <- At.parseOnly oneRef x
+    return $ T.Ref fp k v
+
+-- unexported
+
 oneRef :: At.Parser KeyEntry
--- ^Parses a single isolated reference from a string. This is used for
--- parsing doi dowloads and externally edited references.
+-- ^Parses a single isolated entry from a Text string.
 oneRef = do
-    At.skipWhile ( /= '@' )
-    reference
+    ignored
+    r <- reference
+    At.endOfInput
+    return r
+
+-- =============================================================== --
+-- Unexported helper parsers
 
 ---------------------------------------------------------------------
 -- Parsing references
 
 reference :: At.Parser KeyEntry
--- ^Parses an individual BibTeX reference and consumes all following
--- space until either another reference is encountered or the file
--- ends.
+-- ^Parses an individual BibTeX entry. Metadata associated with the
+-- reference immediately follows the reference entry. All other
+-- comments are ignored.
 reference = do
     (rt, rk) <- refHeader
-    fields <- kvPairs
-    sep <|> At.skipSpace
-    At.char '}'
-    cs <- commentLines
-    At.skipWhile ( /= '@' )
-    return $ ( rk, T.Entry rt fields cs )
+    At.skipSpace
+    fields <- At.many' kvPair
+    -- Entries can end with either a brace or comma-spaces-brace.
+    ( At.char ',' >> At.skipSpace >> At.char '}' ) <|> At.char '}'
+    skipLineSpaces
+    next <- At.peekChar'
+    case next of
+         -- The next reference starts on the same line as the closing brace.
+         '@' -> return ( rk, T.Entry rt fields [] )
+         -- There is meta data after the closing brace.
+         '%' -> do cs <- At.many' metaData
+                   ignored
+                   return ( rk, T.Entry rt fields cs )
+         -- Otherwise there can only be spaces after the closing brace.
+         _   -> do cs <- At.endOfLine >> At.many' metaData
+                   ignored
+                   return ( rk, T.Entry rt fields cs)
 
 ---------------------------------------------------------------------
 -- Parsing reference headers
 
 refHeader :: At.Parser (Text, Text)
--- ^Parses the reference header to obtain the BibTeX reference type
--- such as an 'article' or 'book' and then the reference key. There
--- is no checking for repeated keys.
+-- ^Parses the entry header to obtain the BibTeX entry type such as
+-- an 'article' or 'book' and then the reference key.
 refHeader = do
     rt <- refType
     At.skipSpace
     rk <- refKey
-    At.skipSpace
-    sep
     return (rt, rk)
 
 refType :: At.Parser Text
--- ^Parses the reference type which must have no space between the
--- '@' symbol and the opening brace. For example, '@article{' will
--- parse; however, '@ article{' and '@article {' will not.
+-- ^Parses the reference entry type.
 refType = do
-    At.char '@' <?> "a new BibTeX reference"
-    rt <- At.takeWhile ( At.notInClass "{ " )
+    At.char '@'
+    At.skipSpace
+    rt <- At.takeWhile1 ( At.notInClass "{ \t\n\r\f\v" )
+    At.skipSpace
     At.char '{'
     return rt
 
 refKey :: At.Parser Text
--- ^Parses an reference key, which must be alpha-numeric and may only
--- include the special characters '-', '_', '>' and '<'.
-refKey = pack <$> At.many' ( At.satisfy good )
-    where good x = isAlphaNum x || At.inClass "<>-_" x
+-- ^Parses the reference entry key.
+refKey = At.takeWhile1 ( At.notInClass ",{}@ \t\n\r\f\v" )
 
 ---------------------------------------------------------------------
 -- Parsing key-value pairs
 
-kvPairs :: At.Parser [ T.Field ]
--- ^At.Parser or all key-value pairs in the BibTeX reference fields.
-kvPairs = do
-    firstPair <- kvPair
-    lastPairs <- At.many' ( sep >> kvPair )
-    return $ firstPair : lastPairs
-
 kvPair :: At.Parser T.Field
--- ^At.Parser for individiual key-value pairs in the BibTeX reference
--- fields. Keys must be alpha-numeric, and values must be enclosed by
--- braces and not double quotes.
+-- ^Parse individiual key-value pairs in the BibTeX reference fields.
+-- Values must be enclosed by braces and not double quotes.
 kvPair = do
-    key <- keyParse
-    equals
-    val <- valParse
-    spaces
-    return (key, val)
+    At.char ','
+    At.skipSpace
+    k <- keyParse
+    At.skipSpace
+    At.char '='
+    At.skipSpace
+    v <- valParse
+    At.skipSpace
+    return (k, v)
 
 keyParse :: At.Parser Text
--- ^At.Parser for BibTeX reference field keys.
-keyParse = pack <$> At.many' ( At.satisfy isAlphaNum )
+-- ^Parser for BibTeX reference field keys, which must be alphanumeric
+-- may only contain the alphanumeric '-' or '_' characters.
+keyParse = pack <$> At.many' ( At.satisfy good )
+    where good x = isAlphaNum x || x == '-' || x == '_'
 
 valParse :: At.Parser Text
--- ^At.Parser for BibTeX reference field values. These must be enclosed in
--- braces and not double quotes. Currently, characters of the form
--- '\{' and '\}' are supported only if they are paired. See value
--- parser monad for details.
+-- ^Parser for BibTeX reference field values. These must be enclosed
+-- in braces and not double quotes.
 valParse = do
     At.char '{'
     vs <- value
     return vs
 
 value :: At.Parser Text
--- ^Recursive parser for BibTeX reference field values. Note that
--- even escaped braces (i.e., '\{' and '\}') need to be paired in
--- this implementation.
+-- ^Recursive parser for BibTeX reference field values.
 value = do
-    ls <- pack <$> At.many' ( At.satisfy ( At.notInClass "{}" ) )
-    b  <- At.satisfy ( At.inClass "{}" )
-    case b of
-         '}' -> return ls
-         '{' -> do cs <- value
-                   rs <- value
-                   return $ ls <> ( enbrace cs ) <> rs
+    start <- At.takeWhile ( \ x -> x /= '{' && x /= '}' && x /= '\\' )
+    next  <- At.peekChar'
+    case next of
+         '\\' -> do escaped <- At.take 2
+                    rest    <- value
+                    return $ start <> escaped <> rest
+         '{'  -> do inBraces <- valParse
+                    rest     <- value
+                    return $ start <> ( "{" <> inBraces <> "}" ) <> rest
+         _    -> At.char '}' >> return start
 
 ---------------------------------------------------------------------
--- Parsing comments
+-- Parsing comments and ignored material
 
-commentLines :: At.Parser [ Text ]
--- ^For parsing all comments associated with a reference that will be
--- recorded.
-commentLines = do
-    c <- earlyComment
-    cs <- At.many' commentLine
-    case c of
-         "" -> return cs
-         _  -> return $ c:cs
+ignored :: At.Parser ()
+-- ^Space and comments between entries that should be ignored.
+ignored = At.many' ignoredLine >> At.skipSpace
 
-commentLine :: At.Parser Text
--- ^Parses a recorded comment line.
-commentLine = do
+skipRestOfLine :: At.Parser ()
+skipRestOfLine = At.skipWhile ( not . At.isEndOfLine ) >> At.endOfLine
+
+ignoredLine :: At.Parser ()
+-- ^Empty lines and comments that are ignored.
+ignoredLine = do
+    At.skipSpace
+    At.char '%' >> skipRestOfLine <|> At.endOfLine
+
+metaData :: At.Parser Text
+metaData = do
     At.char '%'
-    whiteSpace
-    s <- pack <$> ( At.manyTill At.anyChar $ eol )
-    whiteSpace
-    return s
-
-earlyComment :: At.Parser Text
--- ^Comment lines that follow a reference closing brace without a new
--- line and are to be recorded.
-earlyComment = do
-    whiteSpace
-    x <- At.peekChar
-    case x of
-         Just '%' -> commentLine
-         Just x   -> eol >> return ""
-         Nothing  -> return ""
+    skipLineSpaces
+    c <- pack <$> ( At.manyTill At.anyChar $ At.endOfLine )
+    skipLineSpaces
+    return c
 
 ---------------------------------------------------------------------
 -- Miscellaneous parsing
 
-eol :: At.Parser Char
--- ^At.Parser for different end of line strings. If the parser succeeds,
--- then the end of line string is consumed and the '\n' character
--- alone is returned. If the parser fails, then nothing is consumed.
-eol = At.choice [ At.string "\r\n" >> return '\n'
-                , At.string "\n\r" >> return '\n'
-                , At.char '\n'
-                ]
-
-sep :: At.Parser ()
--- ^At.Parser for comma separators that consumes all following spaces.
-sep = At.char ',' >> spaces
-
-equals :: At.Parser ()
--- ^At.Parser for equals symbol that consumes all preceeding and
--- following spaces.
-equals = spaces >> At.char '=' >> spaces
-
-whiteSpace :: At.Parser ()
--- ^Spaces but not newlines.
-whiteSpace = At.skipMany $ At.satisfy ( At.inClass " \t" )
-
-emptyLine :: At.Parser ()
--- ^For parsing empty lines that are to be ignored.
-emptyLine = whiteSpace >> eol >> return ()
-
-spaces :: At.Parser ()
-spaces = At.skipMany At.space
-
----------------------------------------------------------------------
--- Nonparsing helper functions
-
-enbrace :: Text -> Text
--- ^Enclose a string in braces.
-enbrace x = "{" <> x <> "}"
+skipLineSpaces :: At.Parser ()
+-- ^Skip spaces and tabs but not newlines.
+skipLineSpaces = At.skipWhile ( \ x -> x == ' ' || x == '\t' )
