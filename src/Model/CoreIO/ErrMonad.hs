264246a525f49eec1178321564e701cbcc86de2f
MWRuszczycky <mwruszczycky@gmail.com>
2019-03-22 21:20:40 -0500

    Removes unnecessary liftIO

--- src/Model/CoreIO/ErrMonad.hs
+++ src/Model/CoreIO/ErrMonad.hs
@@ -19,7 +19,6 @@ import qualified Model.Core.Types    as T
 import Data.Text                             ( Text                )
 import System.Process                        ( callProcess         )
 import System.IO.Error                       ( isDoesNotExistError )
-import Control.Monad.Trans                   ( liftIO              )
 import Control.Monad.Except                  ( ExceptT (..)        )
 import Control.Exception                     ( IOException
                                              , displayException
@@ -31,20 +30,20 @@ import Control.Exception                     ( IOException
 readOrMakeFile :: FilePath -> T.ErrMonad Text
 -- ^Try to read a file, and if it does not exist, then create it.
 readOrMakeFile fp = ExceptT $ do
-    liftIO . catch ( Right <$> Tx.readFile fp ) $ hndlErr
+    catch ( Right <$> Tx.readFile fp ) hndlErr
     where hndlErr :: IOException -> IO ( Either T.ErrString Text )
           hndlErr e | isDoesNotExistError e = pure . Right $ Tx.empty
                     | otherwise             = pure . Left . show $ e
 
 readFileExcept :: FilePath -> T.ErrMonad Text
 readFileExcept fp = ExceptT $ do
-    liftIO . catch ( Right <$> Tx.readFile fp ) $ hndlErr
+    catch ( Right <$> Tx.readFile fp ) hndlErr
     where hndlErr :: IOException -> IO ( Either T.ErrString Text )
           hndlErr = pure . Left . show
 
 writeFileExcept :: FilePath -> Text -> T.ErrMonad ()
 writeFileExcept fp x = ExceptT $ do
-    liftIO . catch ( Right <$> Tx.writeFile fp x ) $ hndlErr
+    catch ( Right <$> Tx.writeFile fp x ) hndlErr
     where hndlErr :: IOException -> IO ( Either T.ErrString () )
           hndlErr = pure . Left . show
 
@@ -54,6 +53,6 @@ writeFileExcept fp x = ExceptT $ do
 callProcExcept :: FilePath -> [String] -> T.ErrMonad ()
 -- ^Wraps an external process inside the ErrMonad monad.
 callProcExcept p args = ExceptT $ do
-    liftIO . catch ( Right <$> callProcess p args ) $ hndlErr
+    catch ( Right <$> callProcess p args ) hndlErr
     where hndlErr :: IOException -> IO ( Either T.ErrString () )
           hndlErr = pure . Left . displayException
